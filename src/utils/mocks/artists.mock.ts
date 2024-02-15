32d051a9a045adefa98a321419d00f70d2c4fb5d
sp3ruzzo <peruzzoarthur@gmail.com>
2024-02-15 19:49:42 -0300

    added timeAdded to artist entity, improved testing a bit

--- src/utils/mocks/artists.mock.ts
+++ src/utils/mocks/artists.mock.ts
@@ -12,6 +12,7 @@ export const genreSavedEntityMock: Genre = {
 
 export const artistCreatedEntityMock: Artist = {
   name: 'The Ogre Magixx',
+  createdAt: '1708026472456',
   genres: [
     {
       id: 'f0fafa21-82d9-4153-9621-cdcfb944484b',
@@ -22,6 +23,7 @@ export const artistCreatedEntityMock: Artist = {
 
 export const artistSavedEntityMock: Artist = {
   name: 'The Ogre Magixx',
+  createdAt: '1708026472456',
   id: '0e944457-8613-4737-b275-be11a0da6127',
   genres: [
     {
@@ -38,11 +40,59 @@ export const artistDtoMock: CreateArtistDto = {
 
 export const artistSavedWithoutGenresMock: Artist = {
   name: 'The Sound of Nothing',
+  createdAt: '1708026472456',
   genres: [],
 };
 
 export const artistCreatedWithoutGenresMock: Artist = {
   name: 'The Sound of Nothing',
+  createdAt: '1708026472456',
   id: '0e944457-8613-4737-b275-be11a0da6667',
   genres: [],
 };
+
+export const arrayOfArtists: Artist[] = [
+  {
+    id: '614b846e-e09c-4f07-95b0-28266e7a87df',
+    name: 'Lithicus Ludicus',
+    createdAt: '1708026472456',
+    genres: [
+      {
+        id: 'ce9a79db-5fc5-422c-833f-d7f91efe5fb1',
+        name: 'Opera Gira',
+      },
+    ],
+  },
+  {
+    id: '032587ac-c470-421e-967e-3779970e406c',
+    name: 'The Ogre Magixx',
+    createdAt: '1708026472456',
+    genres: [
+      {
+        id: 'f0fafa21-82d9-4153-9621-cdcfb944484b',
+        name: 'Ogre Music',
+      },
+    ],
+  },
+  {
+    id: '1796631a-5cbe-4e33-8b2e-e23987be1de7',
+    name: 'Lizza Guizardooo',
+    createdAt: '1708026472456',
+    genres: [
+      {
+        id: '21697560-e991-4635-ab16-0b5572346bc6',
+        name: 'New Era Metal',
+      },
+      {
+        id: 'b4e7003a-21c1-4e9d-b66e-a3e65478380f',
+        name: 'Metalist',
+      },
+    ],
+  },
+  {
+    id: '0e944457-8613-4737-b275-be11a0da6127',
+    name: 'The Sound of Nothing',
+    createdAt: '1708026472456',
+    genres: [],
+  },
+];
--- src/utils/mocks/repositories.mock.ts
+++ src/utils/mocks/repositories.mock.ts
@@ -1,8 +1,16 @@
-import { artistSavedEntityMock, genreSavedEntityMock } from './artists.mock';
+import { Repository } from 'typeorm';
+import {
+  arrayOfArtists,
+  artistSavedEntityMock,
+  genreSavedEntityMock,
+} from './artists.mock';
+import { Artist } from 'src/artists/entities/artist.entity';
 
-export const artistRepositoryMock = {
+export const artistRepositoryMock: Partial<Repository<Artist>> = {
   create: jest.fn().mockResolvedValue(artistSavedEntityMock),
   save: jest.fn().mockReturnValue(artistSavedEntityMock),
+  findOne: jest.fn().mockReturnValue(artistSavedEntityMock),
+  find: jest.fn().mockResolvedValue(arrayOfArtists),
 };
 
 export const genreRepositoryMock = {
