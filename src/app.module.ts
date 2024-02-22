8404ebd869a40a7c5dc0aa2ee3950bdc516675a7
sp3ruzzo <peruzzoarthur@gmail.com>
2024-02-21 23:49:33 -0300

    testing deployment of server

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
-
 import { ConfigModule } from '@nestjs/config';
 import * as Joi from '@hapi/joi';
 import { DatabaseModule } from './database/database.module';
 import { ArtistsModule } from './artists/artists.module';
-// import { UsersModule } from './users/users.module';
 import { GenresModule } from './genres/genres.module';
 
 @Module({
@@ -16,9 +14,6 @@ import { GenresModule } from './genres/genres.module';
         POSTGRES_USER: Joi.string().required(),
         POSTGRES_PASSWORD: Joi.string().required(),
         POSTGRES_DB: Joi.string().required(),
-        JWT_SECRET: Joi.string().required(),
-        JWT_EXPIRATION_TIME: Joi.string().required(),
-        PORT: Joi.number(),
       }),
     }),
     DatabaseModule,
